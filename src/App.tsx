35b83cf31bc1478fdb047794c032a3e1cbb5ff1f
Levente Toth <levente@devprops.com>
2024-08-11 12:28:42 +0200

    Developments

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="site-wrapper">
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+}
