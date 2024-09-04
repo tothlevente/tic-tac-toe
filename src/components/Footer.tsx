c516fc0f35f37793e5e7bf11b8b153ab8e58d3f1
tothlevente <tothlevente139@gmail.com>
2024-09-04 12:44:01 +0200

    Developments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,13 @@
+import packageJson from "../../package.json";
+
 export default function Footer() {
   return (
     <footer>
       <div className="footer-text">
-        <p>Created by Levente in 2024</p>
         <p>This project (website) under MIT license, ad-free and does not use cookies.</p>
         <p>For more information please visit the project repository.</p>
+        <p>Created by Levente in 2024</p>
+        <p>v{packageJson.version}</p>
       </div>
       <div>
         <a className="link" target="_blank" rel="noreferrer" href="https://github.com/tothlevente">
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-
-import Board from "./Board";
 import SquareProps from "../interfaces/SquareProps";
+import Board from "./Board";
+
+import { useState } from "react";
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -22,11 +23,13 @@ export default function Game() {
 
   const moves = history.map((_squares, move) => {
     let description;
+
     if (move > 0) {
       description = "Go to move: " + move + ".";
     } else {
       description = "Go to game start";
     }
+
     return (
       <li className="description-li" key={move}>
         <button className="description-btn" onClick={() => jumpTo(move)}>
