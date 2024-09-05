5d636ff57127b51eecb004b0c92d74dab00d57e9
tothlevente <tothlevente139@gmail.com>
2024-09-05 18:16:53 +0200

    Developments

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Game from "./components/Game";
 
+import { useState } from "react";
+
 export default function App() {
+  const [firstCharacter, setFirstCharacter] = useState("❎");
+  const [secondCharacter, setSecondCharacter] = useState("🔴");
+
   return (
     <div className="site-wrapper">
-      <Header />
+      <Header
+        firstCharacter={firstCharacter}
+        setFirstCharacter={setFirstCharacter}
+        secondCharacter={secondCharacter}
+        setSecondCharacter={setSecondCharacter}
+      />
       <main>
-        <Game />
+        <Game
+          firstCharacter={firstCharacter}
+          secondCharacter={secondCharacter}
+        />
       </main>
       <Footer />
     </div>
---
+++ src/interfaces/GameProps.ts
@@ -0,0 +1,4 @@
+export default interface GameProps {
+  firstCharacter: string;
+  secondCharacter: string;
+}
